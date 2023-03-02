f94c067f87031c849cf6e4e9fe3e5c4c4ee84a20
Markedeath17 <bufi367@gmail.com>
2023-03-02 17:30:49 +0300

    first commit

---
+++ Assets/YourMemory/Scripts/Messanger.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using TMPro;
+using System.Collections.Generic;
+
+public class Messanger : MonoBehaviour
+{
+    private List<TextMeshProUGUI> _texts = new List<TextMeshProUGUI>();
+
+    [SerializeField] private RectTransform _rectTransform;
+    [SerializeField] private TextMeshProUGUI _prefab;
+
+    public void Send(string message)
+    {
+        var variant = Instantiate(_prefab, _rectTransform.position, _rectTransform.rotation, _rectTransform);
+
+        variant.text = message;
+        
+        foreach(var text in _texts)
+        {
+            text.rectTransform.localPosition += new Vector3(0, 50, 0);
+        }
+
+        _texts.Add(variant);
+    }
+}
